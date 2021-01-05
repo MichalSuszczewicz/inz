d27da4a01d3c6cbf07423345b5f3f48be8df1fdd
Michal Suszczewicz <m.suszczewicz@intive.com>
2021-01-05 12:22:42 +0100

    adding objects repository

---
+++ features/objects/android/home_page_objects.py
@@ -0,0 +1,5 @@
+class HomePageObjects():
+	def __init__(self):
+		self.app_title={'id':'','xpath':'/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TextView'}
+		self.accessibility_button={'id':'','xpath':'//android.widget.TextView[@content-desc="Accessibility"]'}
+		self.app_frame={'id':'','xpath':'/hierarchy/android.widget.FrameLayout/android.view.ViewGroup'}
