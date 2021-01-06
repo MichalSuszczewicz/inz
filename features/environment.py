8ec2588264e460325ba437a195fc2ee1fcbe8201
Michal Suszczewicz <m.suszczewicz@intive.com>
2021-01-06 19:20:58 +0100

    fixing drivers

--- features/environment.py
+++ features/environment.py
@@ -2,6 +2,7 @@ from selenium import webdriver as webd
 from appium import webdriver
 from behave import fixture
 from behave import use_fixture
+import os
 
 
 @fixture
@@ -24,16 +25,13 @@ def web(context):
 	context.driver.maximize_window()
 	context.driver.implicitly_wait(10)
 	yield context.driver
-	#context.driver.quit()
+	context.driver.quit()
 
 @fixture
 def mobile(context):
 	dc = {}
-	dc['app'] = '/Users/m.suszczewicz/Study/inz/inz/app/ApiDemos-debug.apk'
-	dc['platformName'] = 'Android'
-	dc['deviceName'] = 'Pixel 3 API 29'
-	dc['automationName'] = 'UiAutomator2'
-	dc["newCommandTimeout"] = 300
+
+
 
 	if 'profile' in context.config.userdata.keys():
 		if context.config.userdata['profile'] is None:
@@ -44,49 +42,32 @@ def mobile(context):
 		profile = 'android'
 
 	if profile == 'android':
+
+		dc['app'] = os.getcwd()+'/app/ApiDemos-debug.apk'
+		dc['platformName'] = 'Android'
+		dc['deviceName'] = 'Pixel 3 API 29'
+		dc['automationName'] = 'UiAutomator2'
+		dc["newCommandTimeout"] = 300
+
 		context.driver = webdriver.Remote("http://localhost:4723/wd/hub", dc)
+
 	elif profile == 'ios':
+		dc['app'] = ''
+		dc['platformName'] = 'iOS'
+		dc['platformVersion'] = ''
+		dc['deviceName'] = ''
+		dc['automationName'] = 'XCUITest'
+		dc["newCommandTimeout"] = 300
 		context.driver = webdriver.Remote("http://localhost:4723/wd/hub", dc)
 	else:
-		context.driver = webdriver.Remote("http://localhost:4723/wd/hub", dc)
+		print('Precise your profile, use android or ios')
 
 	yield context.driver
-	#context.driver.quit()
+	context.driver.quit()
 
-# def before_scenario(context, scenario):
-#
-# 	dc = {}
-# 	dc['app'] = '/Users/m.suszczewicz/Study/inz/inz/app/ApiDemos-debug.apk'
-# 	dc['platformName'] = 'Android'
-# 	dc['deviceName'] = 'Pixel 3 API 29'
-# 	dc['automationName'] = 'UiAutomator2'
-# 	dc["newCommandTimeout"] = 300
-#
-# 	if 'profile' in context.config.userdata.keys():
-# 		if context.config.userdata['profile'] is None:
-# 			profile = 'chrome'
-# 		else:
-# 			profile = context.config.userdata['profile']
-# 	else:
-# 		profile = 'chrome'
-#
-# 	if profile == 'chrome':
-# 		context.driver = webdriver.Chrome()
-# 	elif profile == 'firefox':
-# 		context.driver = webdriver.Firefox()
-# 	elif profile == 'android':
-# 		context.driver = webdriver.Remote("http://localhost:4723/wd/hub", dc)
-# 	else:
-# 		context.driver = webdriver.Chrome()
-# 		context.driver.maximize_window()
-# 		context.driver.implicitly_wait(10)
 
 def before_tag(context, tag):
 	if tag == 'fixture.web':
 		use_fixture(web, context)
 	elif tag == 'fixture.mobile':
 		use_fixture(mobile, context)
-
-
-def after_scenario(context, scenario):
-	context.driver.quit()
--- features/features/web/home.feature
+++ features/features/web/home.feature
@@ -1,13 +1,13 @@
 @fixture.web
 Feature: Open page
 
-    @pass
+    @smoke
     Scenario: Page is accessible and displayed
 
         Given I am on wi zut home page
         Then I see wi zut title "Wydział Informatyki"
-#    @fail
-#    Scenario: Page is accessible and displayed
-#
-#        Given I am on wi zut page
-#        Then I see wi zut title "Wydział Informatykii"
+    @sanity @smoke
+    Scenario: Page is accessible and displayed
+
+        Given I am on wi zut home page
+        Then I see wi zut title "Wydział Informatykii"
