2632c2d8b44441c80a7b0fb1be928b411b30c295
Michal Suszczewicz <m.suszczewicz@intive.com>
2021-01-11 21:55:21 +0100

    adding adb logs for android

--- features/environment.py
+++ features/environment.py
@@ -5,10 +5,12 @@ from behave import use_fixture
 import os
 from datetime import datetime
 from color_logs import *
+import subprocess
 
 
 color = ColorLogs()
 device = ''
+#collect_session = False
 
 @fixture
 def web(context):
@@ -40,6 +42,7 @@ def web(context):
 @fixture
 def mobile(context):
 	dc = {}
+	global collect_session
 
 	if 'profile' in context.config.userdata.keys():
 		if context.config.userdata['profile'] is None:
@@ -56,8 +59,15 @@ def mobile(context):
 		dc['deviceName'] = 'Pixel 3 API 29'
 		dc['automationName'] = 'UiAutomator2'
 		dc["newCommandTimeout"] = 300
+		dc["clearDeviceLogsOnStart"] = True
 		context.driver = webdriver.Remote("http://localhost:4723/wd/hub", dc)
+		logs = context.driver.get_log("logcat")
 		print(color.format('start','===>App starts'))
+		time_start = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+		yield context.driver
+		collect_android_log(context, logs, time_start)
+		context.driver.quit()
+
 
 	elif profile == 'ios':
 		dc['app'] = ''
@@ -68,11 +78,12 @@ def mobile(context):
 		dc["newCommandTimeout"] = 300
 		context.driver = webdriver.Remote("http://localhost:4723/wd/hub", dc)
 		print(color.format('start', '===>App starts'))
+		yield context.driver
+		context.driver.quit()
 	else:
 		print(color.format('fail', 'use one profile from the list: android, ios'))
 
-	yield context.driver
-	context.driver.quit()
+
 	print(color.format('span','Scenario time duration in seconds: '), context.scenario.duration)
 	print(color.format('quit', '===>App quits'))
 
@@ -84,14 +95,27 @@ def before_tag(context, tag):
 	elif tag == "fixture.web":
 		device = "web"
 
+
 def before_scenario(context, scenario):
 	if device == "mobile":
 		use_fixture(mobile, context)
 	elif device == "web":
 		use_fixture(web, context)
 
+
 def after_step(context, step):
 	if step.status == 'failed':
 		now = datetime.now()
 		context.driver.save_screenshot(os.getcwd()+"/screenshots/"+step.name+now.strftime("%m_%d_%Y_%H_%M_%S")+".png")
 		print("screenshot with failed saved in: "+os.getcwd()+"/screenshots/")
+
+
+def collect_android_log(context, logs, time_start):
+	out_filename = os.getcwd() + "/android_logs/logfile_" + context.driver.capabilities.get(
+		'deviceName') + "_" + time_start + ".log"
+	f = open(out_filename, 'w', encoding='utf-8')
+	adb_logs = list(map(lambda log: log['message'], logs))
+	num = len(adb_logs)
+	for i in range(num):
+		f.write(adb_logs[i]+"\n")
+	f.close()
